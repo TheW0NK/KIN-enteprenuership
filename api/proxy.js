635d37021be6ca4e6e9a4ed856a264ecdb7222ca
Adrian Ray LeDeaux <138169736+TheW0NK@users.noreply.github.com>
2024-11-12 16:01:46 +0000

    ...

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
     const targetUrl = 'https://sites.google.com/psdschools.org/wooden?usp=sharing';
     const response = await fetch(targetUrl);
     const body = await response.text();
